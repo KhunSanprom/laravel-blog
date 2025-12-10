705079918f14bf4f962b4a53009494bfd7b7dfc8
KhunSanprom <khun.sanprom@gmail.com>
2025-12-10 16:42:32 +0700

    show blog status

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,8 +6,14 @@
     <h1>BLOGs</h1>
     <hr>
     @foreach ($blogs as $blog)
-        <h4>{{$blog["title"]}}</h4>
-        <p>{{$blog["content"]}}</p>
+        <h4>{{ $blog['title'] }}</h4>
+        <p>{{ $blog['content'] }}</p>
+        
+        @if ($blog['status'] == true)
+            <p class = "text text-success">Publish</p>
+        @else
+            <p class = "text text-danger">Draf</p>
+        @endif
         <hr>
-    @endforeach
-@endsection
+        @endforeach
+    @endsection
